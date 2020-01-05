a3537461c5e0beadd56d5efac13d1648d1a5a3a2
Tom Higgins <tom@gold-i.com>
2020-01-05 16:50:11 +0000

    Change Application Name

--- WhatPetASPC/Controllers/HomeController.cs
+++ WhatPetASPC/Controllers/HomeController.cs
@@ -15,14 +15,14 @@ namespace WhatPetASPC.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "WhatPet - Select your ideal pet friend.";
 
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "Contact Us";
 
             return View();
         }
