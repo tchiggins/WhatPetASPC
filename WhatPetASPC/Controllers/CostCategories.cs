693b9da462f68f05c92ae60294876d6d94fcb98e
Oliver Higgins <oliver@higgins-world.co.uk>
2020-01-14 10:55:46 +0000

    fixed gory merge conflict, added costcategories partial implementation

---
+++ WhatPetASPC/Controllers/CostCategories.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace WhatPetASPC.Models
+{
+    public class CostCategories
+    {
+        [Key]
+        public int CostID { get; set; }
+        public string CostBracket { get; set; }
+    }
+}
